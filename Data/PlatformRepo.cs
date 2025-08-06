2ec7210da07cf90c727c58a2ed2c9ff70e086f0a
BassamRamadan <bassam.ramadan.fci@gmail.com>
2025-08-06 15:48:18 +0300

    Add repository interface and implementation for Platform entity management

---
+++ Data/PlatformRepo.cs
@@ -0,0 +1,45 @@
+
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform? GetPlatformById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null) throw new ArgumentNullException(nameof(platform));
+            _context.Platforms.Add(platform);
+        }
+
+        public void DeletePlatform(int id)
+        {
+            var platform = GetPlatformById(id);
+            if (platform != null)
+            {
+                _context.Platforms.Remove(platform);
+            }
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+    }
+}
